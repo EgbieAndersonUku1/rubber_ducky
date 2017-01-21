479bf36d9638aeaec9aa537860ce8e38712e934e
EgbieAndersonUku1 <jayunderwood2011@hotmail.com>
2017-01-21 20:52:23 +0000

    Update rubber_ducky_UAC_3.0.py

--- rubber_ducky_UAC_3.0.py
+++ rubber_ducky_UAC_3.0.py
@@ -22,7 +22,7 @@
 # CHOICES 3 AND 4 AS THEY REQUIRE A RUBBER DUCKY AND I NO LONGER HAVE A
 # RUBBER DUCKY.
 #
-# WELCOME TO THE MAD HOUSE AND LET THE HACKING BEGIN..
+# WELCOME TO THE MAD HOUSE AND LET THE FUNNNNNNNN BEGIN..
 ##############################################################################
 
 import sys
